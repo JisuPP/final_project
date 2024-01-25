f4adaeb90a5459c40c1dee08f1faefff7286c525
gherwt <gherwt46@gmail.com>
2024-01-25 14:26:36 +0900

    4_sang

--- jeju_olleh/rec_app/urls.py
+++ jeju_olleh/rec_app/urls.py
@@ -6,4 +6,6 @@ app_name = 'rec_app'
 
 urlpatterns = [
     path('', views.recommendation_input, name='recommendation_input'),
+    path('rec_result/', views.rec_result, name='rec_result'),
+    path('detail/<str:destination>/', views.detail, name='detail'),
 ]
--- jeju_olleh/rec_app/views.py
+++ jeju_olleh/rec_app/views.py
@@ -1,13 +1,11 @@
-from django.shortcuts import render
+# rec_app/views.py
 
-# Create your views here.
 from django.shortcuts import render
 from .utils import tokenize_user_input, load_tfidf_matrix, load_tfidf_vectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import os
 from django.conf import settings
-from pathlib import Path
 
 def recommendation_input(request):
     if request.method == 'POST':
@@ -26,6 +24,23 @@ def recommendation_input(request):
         # 결과 DataFrame 생성
         result_df_tfidf = df[['명칭', 'weighted_평점', '개요']].iloc[keyword_input_ind[0, :10]].sort_values('weighted_평점', ascending=False)[:7]
         # 결과를 HTML로 전달
-        return render(request, 'rec_app/recommendation_result.html', {'result_df': result_df_tfidf})
+        return render(request, 'rec_app/rec_result.html', {'result_df': result_df_tfidf})
     return render(request, 'rec_app/recommendation_input.html')
+
+def rec_result(request):
+    # 여기에서 추천 결과 데이터를 가져오도록 수정
+    file_path = os.path.join(settings.BASE_DIR, 'jeju_olleh', 'modules', 'fin_data_weighted_token.csv')
+    df = pd.read_csv(file_path, encoding='utf-8')
+    # 예시로 처음 7개 데이터를 가져옴. 실제로는 어떤 데이터를 가져올지 로직에 맞게 수정 필요
+    result_df = df[['명칭', 'weighted_평점', '개요']].head(7)
+    return render(request, 'rec_app/rec_result.html', {'result_df': result_df})
+
+
+def detail(request, destination):
+    # 여기에서 destination은 선택한 여행지의 명칭입니다.
+    file_path = os.path.join(settings.BASE_DIR, 'jeju_olleh', 'modules', 'fin_data_weighted_token.csv')
+    df = pd.read_csv(file_path, encoding='utf-8')
+    selected_destination = df[df['명칭'] == destination].iloc[0]
+    return render(request, 'rec_app/detail.html', {'selected_destination': selected_destination})
+
 
